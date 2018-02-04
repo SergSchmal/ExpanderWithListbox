a03db8bf07cdfc58da7d3721c27a4a359a7e1969
Sergej Schmal <serg.schmal@gmx.de>
2018-02-04 08:46:42 +0100

    Add project files.

---
+++ ExpanderWithListBox/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ExpanderWithListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Items = new List<string> { "Item 1", "String 2", "Content 3" };
+        }
+
+
+        public List<string> Items { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(ListBoxControl.SelectedItem.ToString());
+        }
+
+        private void ListBoxControl_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var eSource = (ListBox) e.Source;
+            Expander.Header = eSource.SelectedItem.ToString();
+        }
+    }
+}
